8992576f022f47fa55a4d27dc7e817a945257aee
Neo <neodilk@gmail.com>
2025-11-10 10:29:55 +0100

    Updated Website

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -8,11 +8,11 @@ import { GallerySlider } from "@/components/general/gallery";
 
 export const metadata = {
     other: {
-        'og:title': "Brassworks – Create SMP",
+        'og:title': "Brassworks - Create SMP",
         'og:description': "Our public server thrives on cooperation between players - express your creativity freely, with each other.",
         'og:url': "https://brassworks.opnsoc.org/",
         'og:type': "website",
-        'og:image': "https://brassworks.opnsoc.org/images/logo.png",
+        'og:image': "https://brassworks.opnsoc.org/images/icon.png",
     },
 };
 
