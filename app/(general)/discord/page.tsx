8f261093026254d527e8f050a2894aceb9f7db4e
Neo <neodilk@gmail.com>
2025-12-04 12:38:12 +0100

    Update

--- app/(general)/discord/page.tsx
+++ app/(general)/discord/page.tsx
@@ -1,20 +1,5 @@
-import { DiscordRedirect } from "@/components/general/discordredirect";
-
-export const metadata = {
-  title: 'Brassworks - Discord',
-  description:
-    'Join the Brassworks community on Discord - connect with players, get updates, and stay informed about our SMP world.',
-  other: { 
-    'og:title': "Brassworks - Discord",
-    'og:description': "Join the Brassworks community on Discord - connect with players, get updates, and stay informed about our SMP world.",
-    'og:url': "https://brassworks.opnsoc.org/discord",
-    'og:type': "website",
-    'og:image': "https://brassworks.opnsoc.org/images/icon-discord.png",
-  },
-};
+import { redirect } from "next/navigation";
 
 export default function DiscordPage() {
-  return (
-    <DiscordRedirect/>
-  );
+  redirect("https://discord.com/invite/nDhkgzAPR2");
 }
--- components/general/discordredirect.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useEffect } from "react";
-
-export function DiscordRedirect() {
-  useEffect(() => {
-    window.location.href = "https://discord.com/invite/nDhkgzAPR2";
-  }, []);
-}
